616c5dd5e8bebf0223d4a4106356ff691f3b7c55
Isaias Pomales <impomales@gmail.com>
2018-05-04 15:29:50 -0400

    changes after review

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiaW1wb21hbGVzIiwiYSI6ImNqZ3Mzc3p2MTBjZWoycXBtN
 const map = new mapboxgl.Map({
   container: "map",
   center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
-  zoom: 12, // starting zoom
+  zoom: 15, // starting zoom
   style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
 });
 
